92153344ae6827cd696e00e27a6f3944cfd12c0a
wangyb1991 <283577441@qq.com>
2017-09-07 16:14:31 +0800

    first commit

---
+++ javatest/src/com/bonc/javatest/MainTest.java
@@ -0,0 +1,7 @@
+package com.bonc.javatest;
+
+public class MainTest {
+	public static void main(String[] args) {
+		Test.abc('x');
+	}
+}
---
+++ javatest/src/com/bonc/javatest/Test.java
@@ -0,0 +1,16 @@
+package com.bonc.javatest;
+
+public class Test {
+	public static void abc(short a) {
+		System.out.println("short");
+	}
+	public static void abc(int a) {
+		System.out.println("int");
+	}	
+//	public static void abc(double a) {
+//		System.out.println("double");
+//	}	
+//	public static void abc(char a) {
+//		System.out.println("char");
+//	}	
+}
